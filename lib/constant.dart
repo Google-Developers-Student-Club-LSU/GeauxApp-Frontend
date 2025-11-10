b38d577f098d39744a303aa302bc4ee65801e3a7
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2025-11-10 15:47:42 -0600

    Add draggable posts sheet to map page
    Introduces a DraggableScrollableSheet for posts on the map page, with a custom draggable bar and styled container. Refactors constants, updates MapPage to use the new PostsDraggable widget, and prepares for scrollable post view implementation.

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,26 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
 
-const standard_border_radius = 25.0;
+
+const standardBorderRadius = 25.0;
+
+
+const lsuPurple = Color(0xFF461D7C);
+
+
+SliverToBoxAdapter draggableBar = SliverToBoxAdapter(
+              child: Center(
+                child: Center(
+                  child: Container(
+                    height: 4,
+                    width: 100,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                      color: lsuPurple,
+                      borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    ),
+                  ),
+                ),
+              ),
+            );
