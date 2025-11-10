d26742f816d3e00011ab23f2b3b1f44f6336cc05
dtaing11 <157168922+dtaing11@users.noreply.github.com>
2025-11-10 15:56:02 -0600

    Add Google Fonts and path_provider dependencies
    Integrated google_fonts and path_provider packages for enhanced font styling and file system access. Updated iOS and macOS Podfiles and xcconfig files for CocoaPods support, registered path_provider plugin for macOS, and added a reusable title text style using Google Fonts.

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,15 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 const standard_border_radius = 25.0;
+
+
+TextStyle titleTextStyle = GoogleFonts.ptSans(
+  textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 40,
+  ),
+);
